654d65babee00be38de8f893398527b6bb6dfc30
Administrator <administrator@Mac-mini-2.local>
2017-04-09 10:18:28 +0530

    Implemented search bar and search functionality

---
+++ NavigationDrawerDemo/CentreTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CentreTableViewCell.swift
+//  NavigationDrawerDemo
+//
+//  Created by Administrator on 09/04/17.
+//  Copyright © 2017 iFlame. All rights reserved.
+//
+
+import UIKit
+
+class CentreTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cellItemLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- NavigationDrawerDemo/CentreViewController.swift
+++ NavigationDrawerDemo/CentreViewController.swift
@@ -9,16 +9,24 @@
 import UIKit
 import DrawerController
 
-class CentreViewController: UIViewController, UISearchBarDelegate {
+class CentreViewController: UIViewController, UISearchBarDelegate,UITableViewDelegate, UITableViewDataSource {
 
     //lazy   var searchBar:UISearchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: 230, height: 20))
     lazy var searchBar:UISearchBar = UISearchBar()
     var searchBarButtonItem: UIBarButtonItem?
     var label : UILabel!
+     var marrCountryList = [String]()
+    var searchedCountryList = [String]()
     
+    @IBOutlet weak var centreTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
+        marrCountryList = ["USA", "Bahamas", "Brazil", "Canada", "Republic of China", "Cuba", "Egypt", "Fiji", "France", "Germany", "Iceland", "India", "Indonesia", "Jamaica", "Kenya", "Madagascar", "Mexico", "Nepal", "Oman", "Pakistan", "Poland", "Singapore", "Somalia", "Switzerland", "Turkey", "UAE", "Vatican City"]
+        searchedCountryList = marrCountryList
+        centreTableView.delegate = self
+        centreTableView.dataSource = self
+        
     }
     
 
@@ -36,7 +44,7 @@ class CentreViewController: UIViewController, UISearchBarDelegate {
         searchBar.searchBarStyle = UISearchBarStyle.minimal
     }
     
-    //Mark:- Search Bar delegate
+    //MARK:- Search Bar delegate
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         showSearchBar()
     }
@@ -45,7 +53,28 @@ class CentreViewController: UIViewController, UISearchBarDelegate {
         hideSearchBar()
     }
     
-    //Mark:- Menu Search
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("-->> \(searchText)")
+        triggerSearch(searchText: searchText)
+    }
+    
+    
+    //MARK:- Menu Search
+    func triggerSearch(searchText : String){
+        searchedCountryList = []
+        if searchText.characters.count == 0{
+            searchedCountryList = marrCountryList
+        }else{
+            for val in marrCountryList{
+                if val.contains(searchText){
+                    searchedCountryList.append(val)
+                }
+            }
+        }
+        centreTableView.reloadData()
+    }
+    
     func showSearchBar() {
         searchBar.alpha = 0
         navigationItem.titleView = searchBar
@@ -74,7 +103,7 @@ class CentreViewController: UIViewController, UISearchBarDelegate {
         navigationItem.setRightBarButton(searchBarButtonItem, animated: true)
     }
     
-    //Mark:- Navigation label
+    //MARK:- Navigation label
 
     func setupMenuLabel(){
         label = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 18))
@@ -82,7 +111,7 @@ class CentreViewController: UIViewController, UISearchBarDelegate {
         navigationItem.titleView = label
     }
     
-    //Mark:- Left menu
+    //MARK:- Left menu
     
     func setupLeftMenuButton() {
         let leftDrawerButton = DrawerBarButtonItem(target: self, action: #selector(leftDrawerButtonPress(_:)))
@@ -94,5 +123,18 @@ class CentreViewController: UIViewController, UISearchBarDelegate {
         self.evo_drawerController?.toggleDrawerSide(.left, animated: true, completion: nil)
     }
     
+    //MARK:- Table methods
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchedCountryList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CenterTableCellId", for: indexPath) as! CentreTableViewCell
+        cell.cellItemLabel.text = searchedCountryList[indexPath.row]
+        return cell
+    }
 }
